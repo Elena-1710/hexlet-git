66515b99867321fa3121afe85dd01d333a7c40d2
Elena-1710 <zhunina.lena@yandex.ru>
2024-10-19 23:01:59 +0300

    modified index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+/*const fruits = [
+    'apple', 'banana', 'pear',
+    'apricot', 'apple', 'banana',
+    'apple', 'orange', 'pear',
+  ];*/
+  import _ from  'lodash';
+  const text1 = 'one';
+  const result = {};
+  if (text1.length === 0){
+    console.log (result)}
+    else console.log('1');
+  
+  /*const countWorlds = ()
+  const fruitsLowerCase = text1.toLowerCase();
+  const fruits = _.words(text1)*/
+  
+  /*console.log(fruitsLowerCase)
+  console.log (fruits)*/
+
+  /*const countFruits = (fruits) => {
+    const result = {};
+    for (const name of fruits) {
+        if ( Object.hasOwn(result, name)) {
+            result[name]=result[name]+1;
+        } else 
+        result[name]=1;
+    }
+    return result;
+  }
+  console.log (countFruits(fruits))*/
