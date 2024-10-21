622eadfc7ff4732e74f1fce40bc8a25e26891c82
Elena-1710 <zhunina.lena@yandex.ru>
2024-10-21 21:18:52 +0300

    add countWords

--- countWords.js
+++ countWords.js
@@ -1,18 +1,18 @@
-
-
 import _ from 'lodash';
 
 const text1 = 'one two three two ONE one wow';
 const text2 = 'another one sentence with strange Words words';
-const text3= '';
+const text3 = '';
 const countWords = (text) => {
   const textLowerCase = text.toLowerCase();
   const textmassiv = _.words(textLowerCase);
-  //console.log(textmassiv);
+  // console.log(textmassiv);
   const result = {};
-  for (const name of textmassiv)
-  { if (Object.hasOwn(result, name)) { result[name] = result[name] + 1; } else result[name] = 1; }
+  for (const name of textmassiv) {
+    if (Object.hasOwn(result, name)) {
+      result[name] = result[name] + 1;
+    } else result[name] = 1;
+  }
   return result;
 };
 console.log(countWords(text1));
-
